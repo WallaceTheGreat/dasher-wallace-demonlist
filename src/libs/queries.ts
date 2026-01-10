6f4bf37d8af99361d15442548ab5634e11079b64
Wallace <will.ragueneau.dev@gmail.com>
2026-01-10 17:23:17 +0100

    📦 add forms to add levels to the db (not ranking)

--- src/libs/queries.ts
+++ src/libs/queries.ts
@@ -4,3 +4,13 @@ export const getCurrentRankings = () => {
 	const stmt = db.query('SELECT * FROM ranking_view');
 	return stmt.all();
 }
+
+export const getAllLevels = () => {
+	const stmt = db.query('SELECT * FROM levels');
+	return stmt.all();
+}
+
+export const addLevel = (name: string, gd_id: number | null) => {
+	const stmt = db.query('INSERT INTO levels (name, gd_id) VALUES (?, ?)');
+	return stmt.run(name, gd_id);
+}
